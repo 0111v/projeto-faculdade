cce59fb04c475b87b1edcf134b85feabbd77f5f4
oln <victorprg0111@gmail.com>
2025-11-03 11:33:49 -0300

    add shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            O que q a gente vai fazer nesse krai aqui hein? 
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
+            quem tiver alguma sugestao ai se pronuncie kkk{" "}
+            {/* <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
@@ -31,11 +31,11 @@ export default function Home() {
             >
               Learning
             </a>{" "}
-            center.
+            center. */}
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+          {/* <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -57,7 +57,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Documentation
-          </a>
+          </a> */}
         </div>
       </main>
     </div>
